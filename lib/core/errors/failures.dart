871b56c10bebcc1bcca019eab35a58b4c4fb81de
Makarios <makarios.fared@gmail.com>
2024-03-06 22:33:55 +0200

    create server failure from dio error

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,3 +1,58 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errMessage;
+
+  const Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMessage);
+
+  factory ServerFailure.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection timeout');
+
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send timeout');
+
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive timeout');
+
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad certificate');
+
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+            dioError.response!.statusCode!, dioError.response!.data);
+
+      case DioExceptionType.cancel:
+        return ServerFailure('Request cancelled');
+
+      case DioExceptionType.connectionError:
+        return ServerFailure('Connection error');
+
+      case DioExceptionType.unknown:
+        if (dioError.message!.contains('SocketException')) {
+          return ServerFailure('No internet connection');
+        } else {
+          return ServerFailure('Unknown error');
+        }
+      default:
+        return ServerFailure('Unknown error');
+    }
+  }
+
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('Your request Not found , please try again');
+    } else if (statusCode == 500) {
+      return ServerFailure('Internal server error , please try again later');
+    } else {
+      return ServerFailure('Unknown error');
+    }
+  }
+}
