2f96dc2e972f6563b0703368bd90a5d50772d767
Makarios <makarios.fared@gmail.com>
2024-03-11 20:49:27 +0200

    search cubit done

--- lib/Features/search/presentation/manger/search_cubit.dart
+++ lib/Features/search/presentation/manger/search_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bookly_app/Features/home/data/models/Book_model.dart';
+import 'package:bookly_app/Features/search/data/repos/search_repo.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'search_state.dart';
 
 class SearchCubit extends Cubit<SearchState> {
-  SearchCubit() : super(SearchInitial());
+  SearchCubit(this.searchRepo) : super(SearchInitial());
+
+  final SearchRepo searchRepo;
+
+  Future<void> fetchSimilarBooks({required String title}) async {
+    emit(SearchLoading());
+    var result = await searchRepo.fetchBooksBySearchTitle(title: title);
+    result.fold(
+      (failure) {
+        emit(SearchFailure(failure.errMessage));
+      },
+      (books) {
+        emit(SearchSuccess(books));
+      },
+    );
+  }
 }
