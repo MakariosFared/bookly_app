ce1ddcae8796602a27d04c614f120ad1562e9ec6
Makarios <makarios.fared@gmail.com>
2024-03-11 20:32:18 +0200

    create search states

---
+++ lib/Features/search/presentation/manger/search_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bookly_app/Features/home/data/models/Book_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'search_state.dart';
+
+class SearchCubit extends Cubit<SearchState> {
+  SearchCubit() : super(SearchInitial());
+}
