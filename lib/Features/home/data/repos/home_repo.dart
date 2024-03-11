8ad7456fd783b0e1eb742e77c741d7fcbbd3efd6
Makarios <makarios.fared@gmail.com>
2024-03-11 13:55:19 +0200

    implement fetch similar books

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -4,5 +4,9 @@ import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchNewestBooks();
+
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks(
+      {required String category});
 }
